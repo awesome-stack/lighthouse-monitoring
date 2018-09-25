3a8f05f70a729894e116d26f9d48422b5e32ed89
dev <dev@dev.io>
2018-09-26 00:26:09 +0900

    update.

--- reports/index.js
+++ reports/index.js
@@ -1,4 +1,4 @@
-function chartFromJson(jsonData, dirUrl, keyId, titleText, axisY, regions) {
+function chartFromJson(jsonDataArray, dirUrl, keyId, titleText, axisY, regions) {
   var keyArray = [];
   keyArray.push(keyId);
   var regionArray = [];
@@ -29,7 +29,7 @@ function chartFromJson(jsonData, dirUrl, keyId, titleText, axisY, regions) {
     },
     bindto: '#' + keyId,
     data: {
-      json: jsonData,
+      json: jsonDataArray,
       mimeType: 'json',
       keys: {
         x: 'datetime',
@@ -37,7 +37,7 @@ function chartFromJson(jsonData, dirUrl, keyId, titleText, axisY, regions) {
       },
       type: 'spline',
       onclick: function (d, element) {
-        var relativeUrl = dirUrl + '/' + jsonData[d.index]['datetime'] + '_lighthouse.report.html';
+        var relativeUrl = dirUrl + '/' + jsonDataArray[d.index]['datetime'] + '_lighthouse.report.html';
         window.open(relativeUrl, '_blank', 'width=800,height=600');
       }
     },
@@ -60,12 +60,25 @@ function chartFromJson(jsonData, dirUrl, keyId, titleText, axisY, regions) {
 }
 
 function onSelectTarget(name, jsonUrl) {
+  $('#filterDays').dropdown();
   $('#graphArea').show();
   $('#graphTitle').text(name);
   $('title').html(name + ' - Lighthouse Result');
   var dirUrl = jsonUrl.replace('/summary.json', '');
   $.getJSON(jsonUrl,
-    function (jsonData) {
+    function (originalJsonDataArray) {
+      var jsonDataArray = originalJsonDataArray;
+      var filterDays = $('#filterDays').val();
+      console.log(filterDays);
+      if (filterDays !== undefined) {
+        var fromDate = moment().add(filterDays, 'days').format('YYYY-MM-DD');
+        jsonDataArray = originalJsonDataArray.filter(function (item, index) {
+          var iDay = moment(item.datetime + '+09:00');
+          if (iDay.isSameOrAfter(fromDate)) {
+            return true;
+          }
+        });
+      }
       // Score
       var regions4Score = [
         { axis: 'y', start: 90, end: 100, class: 'regionY-green' },
@@ -73,51 +86,51 @@ function onSelectTarget(name, jsonUrl) {
         { axis: 'y', start: 0, end: 49, class: 'regionY-red' },
       ];
       var axisY4Score = { max: 100, min: 0 };
-      chartFromJson(jsonData, dirUrl, 'performance', 'Performance (score)', axisY4Score, regions4Score);
-      chartFromJson(jsonData, dirUrl, 'pwa', 'Progressive Web App (score)', axisY4Score, regions4Score);
-      chartFromJson(jsonData, dirUrl, 'accessibility', 'Accessibility (score)', axisY4Score, regions4Score);
-      chartFromJson(jsonData, dirUrl, 'best-practices', 'Best Practices (score)', axisY4Score, regions4Score);
-      chartFromJson(jsonData, dirUrl, 'seo', 'SEO (score)', axisY4Score, regions4Score);
+      chartFromJson(jsonDataArray, dirUrl, 'performance', 'Performance (score)', axisY4Score, regions4Score);
+      chartFromJson(jsonDataArray, dirUrl, 'pwa', 'Progressive Web App (score)', axisY4Score, regions4Score);
+      chartFromJson(jsonDataArray, dirUrl, 'accessibility', 'Accessibility (score)', axisY4Score, regions4Score);
+      chartFromJson(jsonDataArray, dirUrl, 'best-practices', 'Best Practices (score)', axisY4Score, regions4Score);
+      chartFromJson(jsonDataArray, dirUrl, 'seo', 'SEO (score)', axisY4Score, regions4Score);
       // Detail (TODO: Tuning threshold.)
       var axisY4Detail = { min: 0 };
       // FCP
-      chartFromJson(jsonData, dirUrl, 'first-contentful-paint', 'First Contentful Paint (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'first-contentful-paint', 'First Contentful Paint (ms)', axisY4Detail, [
         { axis: 'y', start: 4500, class: 'regionY-red' },
         { axis: 'y', start: 2500, end: 4499, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 2499, class: 'regionY-green' },
       ]);
       // FMP
-      chartFromJson(jsonData, dirUrl, 'first-meaningful-paint', 'First Meaningful Paint (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'first-meaningful-paint', 'First Meaningful Paint (ms)', axisY4Detail, [
         { axis: 'y', start: 4350, class: 'regionY-red' },
         { axis: 'y', start: 2350, end: 4349, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 2349, class: 'regionY-green' },
       ]);
       // SPDIDX
-      chartFromJson(jsonData, dirUrl, 'speed-index', 'Speed Index (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'speed-index', 'Speed Index (ms)', axisY4Detail, [
         { axis: 'y', start: 6050, class: 'regionY-red' },
         { axis: 'y', start: 3350, end: 6049, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 3349, class: 'regionY-green' },
       ]);
       // FCI
-      chartFromJson(jsonData, dirUrl, 'first-cpu-idle', 'First CPU Idle (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'first-cpu-idle', 'First CPU Idle (ms)', axisY4Detail, [
         { axis: 'y', start: 6550, class: 'regionY-red' },
         { axis: 'y', start: 2350, end: 6549, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 2349, class: 'regionY-green' },
       ]);
       // TTI
-      chartFromJson(jsonData, dirUrl, 'time-to-interactive', 'Time to Interactive (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'time-to-interactive', 'Time to Interactive (ms)', axisY4Detail, [
         { axis: 'y', start: 7550, class: 'regionY-red' },
         { axis: 'y', start: 2350, end: 7549, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 2349, class: 'regionY-green' },
       ]);
       // EIL
-      chartFromJson(jsonData, dirUrl, 'estimated-input-latency', 'Estimated Input Latency (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'estimated-input-latency', 'Estimated Input Latency (ms)', axisY4Detail, [
         { axis: 'y', start: 105, class: 'regionY-red' },
         { axis: 'y', start: 50, end: 104, class: 'regionY-yellow' },
         { axis: 'y', start: 0, end: 49, class: 'regionY-green' },
       ]);
       // TTFB
-      chartFromJson(jsonData, dirUrl, 'time-to-first-byte', 'Time To First Byte (ms)', axisY4Detail, [
+      chartFromJson(jsonDataArray, dirUrl, 'time-to-first-byte', 'Time To First Byte (ms)', axisY4Detail, [
         { axis: 'y', start: 600, class: 'regionY-red' },
         { axis: 'y', start: 0, end: 599, class: 'regionY-green' },
       ]);
@@ -168,4 +181,5 @@ $(function () {
     ]
   });
   $('#graphArea').hide();
+  $('#filterDays').hide();
 });
